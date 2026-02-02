426fe8341374b5f313e7650554b67af23114a452
Sergio Tortosa Benedito <sertorbe@protonmail.com>
2026-02-02 07:48:10 +0100

    More logs

--- src/main.rs
+++ src/main.rs
@@ -287,7 +287,7 @@ impl Bmoji {
 
     fn copy_and_quit(&mut self, glyph: &'static str) -> Task<BmojiMessage> {
         self.options.history.add(glyph.to_string());
-        println!("Copying to clipboard");
+        println!("Copying to clipboard {}", glyph.to_string());
         Task::batch([
             iced::clipboard::write(glyph.to_string()),
             self.save_and_quit(),
